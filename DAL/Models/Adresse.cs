4dd7c87dd969909a829c16233f02c07c6737fba0
Corentin Leboutte <c.leboutte@stag.technofuturtic.education>
2020-09-03 09:40:16 +0200

    Creation modele Adresse

--- DAL/Models/Adresse.cs
+++ DAL/Models/Adresse.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace DAL.Models
 {
-    class Adresse
+    public class Adresse
     {
+        public int AdresseID { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string  Latitude { get; set; }
+
+        public string Rue { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Boite { get; set; }
+
+        public int CodePostal { get; set; }
+
+        public string Ville { get; set; }
+
+        public string Region { get; set; }
+
+        public string Pays { get; set; }
+
     }
 }
