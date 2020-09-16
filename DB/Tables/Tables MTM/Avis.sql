c859d6d006c568aa5ed708d2762c2b367f5ed497
Corentin Leboutte <c.leboutte@stag.technofuturtic.education>
2020-09-16 11:29:52 +0200

    Génération de base de données ok

--- DB/Tables/Tables MTM/Avis.sql
+++ DB/Tables/Tables MTM/Avis.sql
@@ -7,8 +7,9 @@
     [Commentaire] NVARCHAR(320) NOT NULL, 
     [DateCommentaire] DATETIME2 NOT NULL, 
     [isActive] BIT NOT NULL DEFAULT 1, 
-    CONSTRAINT [PK_Avis] PRIMARY KEY ([AvisID]), 
-    CONSTRAINT [PK_AvisID] PRIMARY KEY ([UtilisateurID], ThemeID) ,
+
+    CONSTRAINT [PK_AvisID] PRIMARY KEY (AvisID,[UtilisateurID], ThemeID),
 	CONSTRAINT [FK_UtilisateurTheme_ToUtilisateur] FOREIGN KEY (UtilisateurID) REFERENCES [Utilisateur](UtilisateurID),
 	CONSTRAINT [FK_UtilisateurTheme_ToTheme] FOREIGN KEY (ThemeID) REFERENCES [Theme](ThemeID)
 )
+
