247ac6574db9905116727643c898a613e5a9769a
Corentin Leboutte <c.leboutte@stag.technofuturtic.education>
2020-09-16 11:10:03 +0200

    Procedure stockée create table Avis

---
+++ DB/Procedures/Procedures stockées/SP_Avis/Create_Avis.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Create_Avis]
+	@utilisateurId int,
+	@themeId int,
+	@note int,
+	@commentaire nvarchar(320),
+	@dateCommentaire datetime,
+	@isActive bit
+
+AS
+	BEGIN
+		INSERT INTO Avis(UtilisateurID,ThemeID, Note, Commentaire, DateCommentaire, isActive)
+			VALUES
+				(@utilisateurId, @themeId, @note, @commentaire, @dateCommentaire, @isActive)
+	END
---
+++ DB/Tables/Tables MTM/Avis.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Avis]
+(
+	[AvisID] INT identity  NOT NULL, 
+    [UtilisateurID] INT NOT NULL, 
+    [ThemeID] INT NOT NULL, 
+    [Note] INT NULL, 
+    [Commentaire] NVARCHAR(320) NOT NULL, 
+    [DateCommentaire] DATETIME2 NOT NULL, 
+    [isActive] BIT NOT NULL DEFAULT 1, 
+    CONSTRAINT [PK_Avis] PRIMARY KEY ([AvisID]), 
+    CONSTRAINT [PK_AvisID] PRIMARY KEY ([UtilisateurID], ThemeID) ,
+	CONSTRAINT [FK_UtilisateurTheme_ToUtilisateur] FOREIGN KEY (UtilisateurID) REFERENCES [Utilisateur](UtilisateurID),
+	CONSTRAINT [FK_UtilisateurTheme_ToTheme] FOREIGN KEY (ThemeID) REFERENCES [Theme](ThemeID)
+)
